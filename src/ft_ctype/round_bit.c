02a9a394ff311647233b04fdf189503ac71927c1
CLAIRE BASTIONI <v.bastioni@gmail.com>
2018-06-16 19:29:14 +0200

    fixed round bit for special cases

--- src/ft_ctype/round_bit.c
+++ src/ft_ctype/round_bit.c
@@ -3,25 +3,47 @@
 int             round_bit(int n)
 {
     int         i;
+    int         b;
+    int         neg;
 
     i = 0;
+    b = 0;
+    if (n == 0)
+        return (0);
+    neg = n < 0;
+    n = neg ? -n : n;
+    if (n < 0)
+        return (n);
     while (n != 0)
     {
         i++;
+        b += (n & 1);
         n >>= 1;
     }
-    return (1 << i);
+    n = (b == 1 ? (1 << (i - 1)) : (1 << i));
+    return (neg ? -n : n);
 }
 
 long            round_bit_long(long n)
 {
     long        l;
+    int         b;
+    int         neg;
 
     l = 0;
+    b = 0;
+    if (n == 0)
+        return (0);
+    neg = n < 0;
+    n = neg ? -n : n;
+    if (n < 0)
+        return (n);
     while (n != 0)
     {
         l++;
+        b += (n & 1);
         n >>= 1;
     }
-    return (1 << l);
+    n = (b == 1 ? (1 << (l - 1)) : (1 << l));
+    return (neg ? -n : n);
 }
